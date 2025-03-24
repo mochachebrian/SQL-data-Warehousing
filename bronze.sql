d2a8688f6dce2d2d950a8f435d9bfc934df30bba
mochache <mochachebrian84@gmail.com>
2025-03-25 01:20:03 +0300

    Update bronze.sql

--- bronze.sql
+++ bronze.sql
@@ -1,1 +1,27 @@
+CREATE DATABASE mokey_pox;
+USE mokey_pox;
+
+CREATE TABLE mokey_pox.monkey_pox (
+    location VARCHAR(100),
+    iso_code VARCHAR(10),
+    date DATE,
+    total_cases INT,
+    total_deaths INT,
+    new_cases INT,
+    new_deaths INT,
+    new_cases_smoothed FLOAT,
+    new_deaths_smoothed FLOAT,
+    new_cases_per_million FLOAT,
+    total_cases_per_million FLOAT,
+    new_cases_smoothed_per_million FLOAT,
+    new_deaths_per_million FLOAT,
+    total_deaths_per_million FLOAT,
+    new_deaths_smoothed_per_million FLOAT
+);
+
+-- LOADING CSV INTO THE DATABASE 
+
+
+
+
 
